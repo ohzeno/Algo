e64bd013cd418e87d1d24def732cbb7b4b323aa4
ohzeno <blackroseaaa3@gmail.com>
2022-06-25 08:07:45 +0900

    Math S5

---
+++ Math/BJ/Silver/S5/bj_1978/bj_1978.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/11050
+import sys
+sys.stdin = open('input.txt')
+def input():
+    return sys.stdin.readline().rstrip()
+
+n = int(input())
+datas = list(map(int, input().split()))
+ans = 0
+for data in datas:
+    if data == 1:  # 1은 소수 아님. 다음 숫자.
+        continue
+    for i in range(2, data):  # 1과 자신 제외한 수
+        if data % i == 0:  # 나누어 떨어지면 소수 아님
+            break
+    else:  # break 안됐으면 소수
+        ans += 1
+print(ans)
