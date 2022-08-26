cffb3d4adb4a4bd0f4eca5c5ccdfae3366e6c62f
ohzeno <blackroseaaa3@gmail.com>
2022-08-26 18:41:36 +0900

    String B5

---
+++ BaekJoon/String/Bronze/B5/bj_25083/bj_25083.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/25083
+import sys
+sys.stdin = open('input.txt')
+def input():
+    return sys.stdin.readline().rstrip()
+
+print('         ,r\'"7')
+print('r`-_   ,\'  ,/')
+print(' \. ". L_r\'')
+print('   `~\/')
+print('      |')
+print('      |')
