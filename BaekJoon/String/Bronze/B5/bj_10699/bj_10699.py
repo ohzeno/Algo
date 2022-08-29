a862e553bdfcd6bd0f014508668db0a20fb6e1a2
ohzeno <blackroseaaa3@gmail.com>
2022-08-29 20:44:25 +0900

    String B5

--- BaekJoon/String/Bronze/B5/bj_10699/bj_10699.py
+++ BaekJoon/String/Bronze/B5/bj_10699/bj_10699.py
@@ -1,6 +1,2 @@
 # https://www.acmicpc.net/problem/10699
-import sys
-sys.stdin = open('input.txt')
-def input():
-    return sys.stdin.readline().rstrip()
 print('2022-08-28')
--- BaekJoon/String/Bronze/B5/bj_25083/bj_25083.py
+++ BaekJoon/String/Bronze/B5/bj_25083/bj_25083.py
@@ -1,9 +1,4 @@
 # https://www.acmicpc.net/problem/25083
-import sys
-sys.stdin = open('input.txt')
-def input():
-    return sys.stdin.readline().rstrip()
-
 print('         ,r\'"7')
 print('r`-_   ,\'  ,/')
 print(' \. ". L_r\'')
