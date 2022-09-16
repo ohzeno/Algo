02c2535ed74484679638d859ada9bd409e8e84d9
ohzeno <blackroseaaa3@gmail.com>
2022-09-16 19:04:40 +0900

    Implementation B5

---
+++ BaekJoon/Implementation/Bronze/B5/bj_15964/bj_15964.py
@@ -0,0 +1,8 @@
+# https://www.acmicpc.net/problem/15964
+import sys
+sys.stdin = open('input.txt')
+def input():
+    return sys.stdin.readline().rstrip()
+
+a, b = map(int, input().split())
+print((a+b)*(a-b))
