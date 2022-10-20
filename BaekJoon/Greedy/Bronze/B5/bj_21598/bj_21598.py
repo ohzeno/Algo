9f7497bde1f1db2bffe7e93959dc1c77f40e92cb
ohzeno <blackroseaaa3@gmail.com>
2022-10-20 16:50:31 +0900

    Greedy B5

---
+++ BaekJoon/Greedy/Bronze/B5/bj_21598/bj_21598.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/21598
+import sys
+sys.stdin = open('input.txt')
+def input():
+    return sys.stdin.readline().rstrip()
+
+a = int(input())
+for i in range(a):
+    print("SciComLove")
