7b08e1caf98c5e8cf4b681ed4cf4634bb3196064
Zeno Oh <blackroseaaa3@gmail.com>
2023-05-18 09:08:50 +0900

    SQL JOIN Lv.2

---
+++ SQL/Programmers/JOIN/Lv.2/상품 별 오프라인 매출 구하기.sql
@@ -0,0 +1,13 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131533
+/*
+PRODUCT 테이블과 OFFLINE_SALE 테이블에서
+상품코드 별 매출액(판매가 * 판매량) 합계를 출력하는 SQL문을 작성해주세요.
+결과는 매출액을 기준으로 내림차순 정렬해주시고
+매출액이 같다면 상품코드를 기준으로 오름차순 정렬해주세요.
+*/
+SELECT PRO.PRODUCT_CODE, SUM(PRO.PRICE * OFF.SALES_AMOUNT) AS SALES
+FROM PRODUCT AS PRO
+JOIN OFFLINE_SALE AS OFF  # JOIN = INNER JOIN 양쪽 테이블에서 일치하는 데이터만 가져옴. LEFT JOIN은 오른쪽 테이블에 값 없으면 NULL로 출력.
+    ON PRO.PRODUCT_ID = OFF.PRODUCT_ID
+GROUP BY PRO.PRODUCT_CODE  # 상품코드 별
+ORDER BY SALES DESC, PRO.PRODUCT_CODE ASC;
