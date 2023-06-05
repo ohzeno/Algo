97879301c4b855d509ad75314b68f62d9769b8b4
Zeno Oh <blackroseaaa3@gmail.com>
2023-06-05 14:07:19 +0900

    SQL JOIN Lv.4

---
+++ SQL/Programmers/JOIN/Lv.4/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -0,0 +1,25 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131124
+/*
+MEMBER_PROFILE와 REST_REVIEW 테이블에서 리뷰를 가장 많이 작성한 회원의 리뷰들을 조회하는 SQL문을 작성해주세요.
+회원 이름, 리뷰 텍스트, 리뷰 작성일이 출력되도록 작성해주시고,
+결과는 리뷰 작성일을 기준으로 오름차순,
+리뷰 작성일이 같다면 리뷰 텍스트를 기준으로 오름차순 정렬해주세요.
+*/
+WITH MOST_REVIEW_MEMBER AS (  # 리뷰를 가장 많이 작성한 회원
+    SELECT MEMBER_ID, COUNT(*) AS REVIEW_COUNT
+    FROM REST_REVIEW
+    GROUP BY MEMBER_ID
+    ORDER BY REVIEW_COUNT DESC
+    LIMIT 1
+), REVIEWS AS (  # 그 회원이 작성한 리뷰들
+    SELECT REVIEW.MEMBER_ID, REVIEW.REVIEW_TEXT, REVIEW.REVIEW_DATE
+    FROM REST_REVIEW AS REVIEW
+    JOIN MOST_REVIEW_MEMBER AS MOST
+        ON REVIEW.MEMBER_ID = MOST.MEMBER_ID
+)
+SELECT INFO.MEMBER_NAME, REVIEWS.REVIEW_TEXT,
+       DATE_FORMAT(REVIEWS.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+FROM MEMBER_PROFILE AS INFO
+JOIN REVIEWS  # 이름 가져와서 합쳐줌.
+    ON INFO.MEMBER_ID = REVIEWS.MEMBER_ID
+ORDER BY REVIEWS.REVIEW_DATE ASC, REVIEWS.REVIEW_TEXT ASC
