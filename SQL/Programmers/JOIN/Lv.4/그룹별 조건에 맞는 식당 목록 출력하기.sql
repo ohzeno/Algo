1a082e1fe1c972a7d7982de795e9498ca6c21163
Zeno Oh <blackroseaaa3@gmail.com>
2023-09-19 15:28:49 +0900

    SQL JOIN Lv.4 repeat

--- SQL/Programmers/JOIN/Lv.4/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ SQL/Programmers/JOIN/Lv.4/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -5,21 +5,29 @@ MEMBER_PROFILE와 REST_REVIEW 테이블에서 리뷰를 가장 많이 작성한 
 결과는 리뷰 작성일을 기준으로 오름차순,
 리뷰 작성일이 같다면 리뷰 텍스트를 기준으로 오름차순 정렬해주세요.
 */
-WITH MOST_REVIEW_MEMBER AS (  # 리뷰를 가장 많이 작성한 회원
-    SELECT MEMBER_ID, COUNT(*) AS REVIEW_COUNT
-    FROM REST_REVIEW
-    GROUP BY MEMBER_ID
-    ORDER BY REVIEW_COUNT DESC
-    LIMIT 1
-), REVIEWS AS (  # 그 회원이 작성한 리뷰들
-    SELECT REVIEW.MEMBER_ID, REVIEW.REVIEW_TEXT, REVIEW.REVIEW_DATE
-    FROM REST_REVIEW AS REVIEW
-    JOIN MOST_REVIEW_MEMBER AS MOST
-        ON REVIEW.MEMBER_ID = MOST.MEMBER_ID
-)
-SELECT INFO.MEMBER_NAME, REVIEWS.REVIEW_TEXT,
-       DATE_FORMAT(REVIEWS.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
-FROM MEMBER_PROFILE AS INFO
-JOIN REVIEWS  # 이름 가져와서 합쳐줌.
-    ON INFO.MEMBER_ID = REVIEWS.MEMBER_ID
-ORDER BY REVIEWS.REVIEW_DATE ASC, REVIEWS.REVIEW_TEXT ASC
+# 리뷰를 가장 많이 작성한 회원
+WITH MAX_REV_MEMBER AS (SELECT MEMBER_ID, COUNT(*) AS REVIEW_CNT
+                        FROM REST_REVIEW
+                        GROUP BY MEMBER_ID
+                        ORDER BY REVIEW_CNT DESC
+                        LIMIT 1),
+    # 그 회원이 작성한 리뷰들
+     REVIEWS AS (SELECT REV.MEMBER_ID, REV.REVIEW_TEXT, REV.REVIEW_DATE
+                 FROM MAX_REV_MEMBER AS MEM
+                          JOIN REST_REVIEW AS REV
+                               ON MEM.MEMBER_ID = REV.MEMBER_ID)
+SELECT PRO.MEMBER_NAME,
+       REV.REVIEW_TEXT,
+       DATE_FORMAT(REV.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+FROM MEMBER_PROFILE AS PRO
+         JOIN REVIEWS AS REV # 이름 가져와서 합쳐줌.
+              ON PRO.MEMBER_ID = REV.MEMBER_ID
+ORDER BY REV.REVIEW_DATE, REV.REVIEW_TEXT
+
+
+/*
+WITH를 사용하지 않고 해보려다가 서브쿼리가 너무 복잡해서 그냥 WITH를 사용했다.
+이전과는 명칭들만 조금씩 달라졌다.
+리뷰를 가장 많이 작성한 회원이 여럿이 나오긴 하는데,
+문제에서 '회원들'이 아니라 '회원'이라고 해서 LIMIT을 사용했다.
+*/
