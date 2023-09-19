3ec07dbb17fa829322500b31623042f364ac9bc9
Zeno Oh <blackroseaaa3@gmail.com>
2023-09-19 14:17:23 +0900

    SQL JOIN Lv.3 repeat

--- SQL/Programmers/JOIN/Lv.3/오랜 기간 보호한 동물(1).sql
+++ SQL/Programmers/JOIN/Lv.3/오랜 기간 보호한 동물(1).sql
@@ -4,10 +4,18 @@
 가장 오래 보호소에 있었던 동물 3마리의 이름과 보호 시작일을 조회하는 SQL문을 작성해주세요.
 이때 결과는 보호 시작일 순으로 조회해야 합니다.
 */
-SELECT INS.NAME, INS.DATETIME  # 입양 못 간 동물이니 보호소 기반.
-FROM ANIMAL_INS INS
-LEFT JOIN ANIMAL_OUTS OUTS
-    ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
-WHERE OUTS.ANIMAL_ID IS NULL  # 입양 못 간 동물
-ORDER BY INS.DATETIME  # 보호 시작일 순
-LIMIT 3  # 3마리만
+SELECT INS.NAME, INS.DATETIME # 입양 못 간 동물이니 보호소 기반.
+FROM ANIMAL_INS AS INS
+         LEFT JOIN ANIMAL_OUTS AS OUTS
+                   ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE OUTS.ANIMAL_ID IS NULL # 입양 못 간 동물
+# ORDER BY INS.DATETIME # 보호 시작일 순
+ORDER BY DATETIME
+LIMIT 3 # 3마리만
+
+
+/*
+다시 풀면서 ORDER BY만 바뀌었다.
+OUTS에도 DATETIME이 존재하니 원래는 INS.으로 특정해줘야 하지만
+이 경우는 OUTS에 없는 것만 조회하니 DATETIME은 INS의 컬럼으로 특정된다.
+*/
