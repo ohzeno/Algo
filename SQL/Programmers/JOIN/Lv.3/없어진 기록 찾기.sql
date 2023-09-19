87cc03e82cf5d8acf133614282ae96bf726fb077
Zeno Oh <blackroseaaa3@gmail.com>
2023-09-19 14:09:43 +0900

    SQL JOIN Lv.3 repeat

--- SQL/Programmers/JOIN/Lv.3/없어진 기록 찾기.sql
+++ SQL/Programmers/JOIN/Lv.3/없어진 기록 찾기.sql
@@ -5,9 +5,14 @@
 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
 ANIMAL_OUTS 테이블의 ANIMAL_ID는 ANIMAL_INS의 ANIMAL_ID의 외래 키입니다.
 */
-SELECT OUTS.ANIMAL_ID, OUTS.NAME  # 입양 간 기록이 있으므로 OUT 기반
-FROM ANIMAL_OUTS OUTS
-LEFT JOIN ANIMAL_INS INS
-    ON OUTS.ANIMAL_ID = INS.ANIMAL_ID
-WHERE INS.ANIMAL_ID IS NULL  # INS는 추가 테이블이므로 보호소에 들어온 기록이 없으면 NULL
-ORDER BY OUTS.ANIMAL_ID
+SELECT OUTS.ANIMAL_ID, OUTS.NAME # 입양 간 기록이 있으므로 OUT 기반
+FROM ANIMAL_OUTS AS OUTS
+         LEFT JOIN ANIMAL_INS AS INS
+                   ON OUTS.ANIMAL_ID = INS.ANIMAL_ID
+WHERE INS.ANIMAL_ID IS NULL # INS는 추가 테이블이므로 보호소에 들어온 기록이 없으면 NULL
+ORDER BY OUTS.ANIMAL_ID;
+
+/*
+다시 풀면서 AS만 추가됐다.
+생략해도 되지만 가독성 면에서 넣는 것이 좋아 보였다.
+*/
