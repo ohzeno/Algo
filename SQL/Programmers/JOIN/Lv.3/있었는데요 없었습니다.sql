869821d7a4ce62e013b04b5e7830ab6eed25f0ce
Zeno Oh <blackroseaaa3@gmail.com>
2023-09-19 14:22:17 +0900

    SQL JOIN Lv.3 repeat

--- SQL/Programmers/JOIN/Lv.3/있었는데요 없었습니다.sql
+++ SQL/Programmers/JOIN/Lv.3/있었는데요 없었습니다.sql
@@ -5,9 +5,21 @@
 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
 ANIMAL_OUTS 테이블의 ANIMAL_ID는 ANIMAL_INS의 ANIMAL_ID의 외래 키입니다.
 */
-SELECT INS.ANIMAL_ID, INS.NAME  # 양쪽에 다 있는 값 가져올거라 기반 outs해도 됨.
-FROM ANIMAL_OUTS OUTS
-LEFT JOIN ANIMAL_INS INS
-    ON OUTS.ANIMAL_ID = INS.ANIMAL_ID
-WHERE OUTS.DATETIME < INS.DATETIME  # 보호 시작일보다 입양일이 더 빠른 동물
-ORDER BY INS.DATETIME  # 보호 시작일 순
+# SELECT INS.ANIMAL_ID, INS.NAME # 양쪽에 다 있는 값 가져올거라 기반 outs해도 됨.
+# FROM ANIMAL_OUTS OUTS
+#          LEFT JOIN ANIMAL_INS INS
+#                    ON OUTS.ANIMAL_ID = INS.ANIMAL_ID
+# WHERE OUTS.DATETIME < INS.DATETIME # 보호 시작일보다 입양일이 더 빠른 동물
+# ORDER BY INS.DATETIME; # 보호 시작일 순
+
+SELECT INS.ANIMAL_ID, INS.NAME
+FROM ANIMAL_INS AS INS
+         LEFT JOIN ANIMAL_OUTS AS OUTS
+                   ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE INS.DATETIME > OUTS.DATETIME
+ORDER BY INS.DATETIME
+
+/*
+다시 풀면서 기반이 INS로 바뀌었다.
+입양나간 날짜가 잘못되었다고 해서 잘못되지 않은 정보가 있는 INS를 기반으로 했다.
+*/
