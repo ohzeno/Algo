0e4d60cf7c11f919330ea71f2a4531023d20f90b
Zeno Oh <blackroseaaa3@gmail.com>
2023-09-21 03:05:34 +0900

    SQL JOIN Lv.4 repeat

--- SQL/Programmers/JOIN/Lv.4/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ SQL/Programmers/JOIN/Lv.4/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -11,27 +11,52 @@ CAR_RENTAL_COMPANY_DISCOUNT_PLAN 테이블에서
 대여 금액이 같은 경우 자동차 종류를 기준으로 오름차순 정렬,
 자동차 종류까지 같은 경우 자동차 ID를 기준으로 내림차순 정렬해주세요.
 */
-WITH AVAILABE_CARS AS (
-    SELECT CAR_ID, CAR_TYPE, DAILY_FEE AS FEE
-    FROM CAR_RENTAL_COMPANY_CAR AS CAR
-    WHERE CAR_TYPE IN ('세단', 'SUV')  # 종류가 세단, SUV
-      AND CAR_ID NOT IN (  # 2022년 11월 1일부터 2022년 11월 30일까지 대여 가능
-        SELECT CAR_ID
-        FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-        WHERE START_DATE BETWEEN '2022-11-01' AND '2022-11-30'  # 시작일이 11월 1일부터 11월 30일 사이
-           OR END_DATE BETWEEN '2022-11-01' AND '2022-11-30'  # 종료일이 11월 1일부터 11월 30일 사이
-           OR '2022-11-01' BETWEEN START_DATE AND END_DATE  # 예를 들어 10.01~12.01인 경우 위 조건을 만족해도 대여 불가
-      )
-), DISCOUNTED_FEES AS (
-    SELECT
-        AC.CAR_ID, AC.CAR_TYPE,
-        AC.FEE * (100 - DP.DISCOUNT_RATE)/100 * 30 AS FEE  # 할인한 가격으로 30일 대여비용
-    FROM AVAILABE_CARS AS AC
-    JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS DP
-        ON AC.CAR_TYPE = DP.CAR_TYPE
-               AND DP.DURATION_TYPE = '30일 이상'  # 30일 대여할 거라서 이것만
-)
-SELECT CAR_ID, CAR_TYPE, CAST(FEE AS SIGNED INT) AS FEE  # 그냥 INT하면 오류남. 부호 있는 정수로 캐스팅.
-FROM DISCOUNTED_FEES
-WHERE FEE BETWEEN 500000 AND 1999999  # BETWEEN은 []임. 양끝 포함.
-ORDER BY FEE DESC, CAR_TYPE ASC, CAR_ID DESC
+# WITH AVAILABE_CARS AS (SELECT CAR_ID, CAR_TYPE, DAILY_FEE AS FEE
+#                        FROM CAR_RENTAL_COMPANY_CAR AS CAR
+#                        WHERE CAR_TYPE IN ('세단', 'SUV') # 종류가 세단, SUV
+#                          AND CAR_ID NOT IN ( # 2022년 11월 1일부터 2022년 11월 30일까지 대여 가능
+#                            SELECT CAR_ID
+#                            FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+#                            WHERE START_DATE BETWEEN '2022-11-01' AND '2022-11-30' # 시작일이 11월 1일부터 11월 30일 사이
+#                               OR END_DATE BETWEEN '2022-11-01' AND '2022-11-30'   # 종료일이 11월 1일부터 11월 30일 사이
+#                               OR '2022-11-01' BETWEEN START_DATE AND END_DATE # 예를 들어 10.01~12.01인 경우 위 조건을 만족해도 대여 불가
+#                        )),
+#      DISCOUNTED_FEES AS (SELECT AC.CAR_ID,
+#                                 AC.CAR_TYPE,
+#                                 AC.FEE * (100 - DP.DISCOUNT_RATE) / 100 * 30 AS FEE # 할인한 가격으로 30일 대여비용
+#                          FROM AVAILABE_CARS AS AC
+#                                   JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS DP
+#                                        ON AC.CAR_TYPE = DP.CAR_TYPE
+#                                            AND DP.DURATION_TYPE = '30일 이상' # 30일 대여할 거라서 이것만
+#      )
+# SELECT CAR_ID, CAR_TYPE, CAST(FEE AS SIGNED INT) AS FEE # 그냥 INT하면 오류남. 부호 있는 정수로 캐스팅.
+# FROM DISCOUNTED_FEES
+# WHERE FEE BETWEEN 500000 AND 1999999 # BETWEEN은 []임. 양끝 포함.
+# ORDER BY FEE DESC, CAR_TYPE ASC, CAR_ID DESC;
+
+WITH CANT_RENT AS (SELECT CAR_ID
+                   FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                   WHERE '2022-11.01' BETWEEN START_DATE AND END_DATE
+                      OR '2022-11-30' BETWEEN START_DATE AND END_DATE),
+     DISCOUNT AS (SELECT CAR_TYPE, DISCOUNT_RATE
+                  FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+                  WHERE CAR_TYPE IN ('세단', 'SUV')
+                    AND DURATION_TYPE = '30일 이상'),
+     TOTAL_FEE AS (SELECT DAY.CAR_ID,
+                          DAY.CAR_TYPE,
+                          FLOOR(DAY.DAILY_FEE * 30 * (100 - DC.DISCOUNT_RATE) / 100) AS FEE
+                   FROM CAR_RENTAL_COMPANY_CAR AS DAY
+                            JOIN DISCOUNT AS DC
+                                 ON DAY.CAR_TYPE = DC.CAR_TYPE
+                   WHERE DAY.CAR_ID NOT IN (SELECT * FROM CANT_RENT))
+SELECT CAR_ID, CAR_TYPE, FEE
+FROM TOTAL_FEE
+WHERE 500000 <= FEE
+  AND FEE < 2000000
+ORDER BY FEE DESC, CAR_TYPE, CAR_ID
+
+/*
+다시 풀면서 WITH문이 하나 늘었다. CAST 대신 FLOOR를 사용했고
+금액에 대해 BETWEEN 대신 부등호를 사용했다.
+가독성이 훨씬 올라간듯.
+*/
