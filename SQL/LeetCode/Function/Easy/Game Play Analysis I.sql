bf4b4e3581705921a554e21725a1e48cacd3a01a
Zeno Oh <blackroseaaa3@gmail.com>
2023-10-08 20:09:52 +0900

    SQL Function Easy

---
+++ SQL/LeetCode/Function/Easy/Game Play Analysis I.sql
@@ -0,0 +1,16 @@
+-- https://leetcode.com/problems/game-play-analysis-i/?envType=study-plan&id=sql-i
+Create table If Not Exists Activity (player_id int, device_id int, event_date date, games_played int);
+Truncate table Activity;
+insert into Activity (player_id, device_id, event_date, games_played)
+values ('1', '2', '2016-03-01', '5'),
+       ('1', '2', '2016-05-02', '6'),
+       ('2', '3', '2017-06-25', '1'),
+       ('3', '1', '2016-03-02', '0'),
+       ('3', '4', '2018-07-03', '5');
+/*
+Write an SQL query to report the first login date for each player.
+Return the result table in any order.
+*/
+SELECT player_id, MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id;
