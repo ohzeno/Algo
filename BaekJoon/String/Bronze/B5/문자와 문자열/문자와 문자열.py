d5a73377671bd3455b2d5597e0d5941b817b4b3d
Zeno Oh <blackroseaaa3@gmail.com>
2023-11-08 07:41:54 +0900

    String B5

---
+++ BaekJoon/String/Bronze/B5/문자와 문자열/문자와 문자열.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/27866
+import sys
+sys.stdin = open('input.txt')
+def input():
+    return sys.stdin.readline().rstrip()
+"""
+"""
+s = input()
+n = int(input())
+print(s[n-1])
+
+
+"""
+현 시점 브론즈 5. 제출 43469. 정답률 68.477 %
+백준 1클래스의 휘장이 사라졌길래 다시 보니 새로운 문제가 추가되어 있었다.
+간단한 인덱싱 문제.
+"""
