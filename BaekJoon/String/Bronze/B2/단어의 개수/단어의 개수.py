245bfe349a47ee8741b7c6adbf889868c24d1e27
Zeno Oh <blackroseaaa3@gmail.com>
2024-09-29 07:50:00 +0900

    String B2

---
+++ BaekJoon/String/Bronze/B2/단어의 개수/단어의 개수.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/1152
+import sys
+# sys.stdin = open('input.txt')
+def input():
+    return sys.stdin.readline().rstrip()
+"""
+"""
+
+a = list(input().split())
+print(len(a))
+
+"""
+현 시점 Bronze II. 제출 416798. 정답률 33.181 %
+예전에 풀었는데 레포에는 없어서 다시 풀었다.
+"""
