d75e3d4a48126b7d584384fbd463d260683e95b4
Zeno Oh <blackroseaaa3@gmail.com>
2024-10-01 10:28:33 +0900

    Implementation B5

---
+++ BaekJoon/Implementation/Bronze/B5/별 찍기 - 1/별 찍기 - 1.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/2438
+import sys
+# sys.stdin = open('input.txt')
+def input():
+    return sys.stdin.readline().rstrip()
+"""
+"""
+
+n = int(input())
+for i in range(1, n+1):
+    print('*' * i)
+
+
+"""
+현 시점 Bronze V. 제출 352228. 정답률 62.171 %
+예전에 풀었는데 레포에는 없어서 다시 풀었다.
+"""
