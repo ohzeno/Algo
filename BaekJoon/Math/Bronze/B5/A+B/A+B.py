df536e36677e745bdd5aa9ab2105b624e167e918
Zeno Oh <blackroseaaa3@gmail.com>
2024-10-05 08:34:10 +0900

    Math B5

---
+++ BaekJoon/Math/Bronze/B5/A+B/A+B.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/1000
+import sys
+# sys.stdin = open('input.txt')
+def input():
+    return sys.stdin.readline().rstrip()
+"""
+"""
+
+a, b = map(int, input().split())
+print(a+b)
+
+"""
+현 시점 Bronze V. 제출 1161377. 정답률 38.813 %
+예전에 풀었는데 레포에는 없어서 다시 풀었다.
+"""
