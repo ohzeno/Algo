5ff219e65481a21890ab269cde415172f6b92593
Zeno Oh <blackroseaaa3@gmail.com>
2024-10-14 15:42:34 +0900

    Add JavaScript form for LeetCode

---
+++ Utils/Forms/리트코드 양식.js
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/count-number-of-nice-subarrays/
+
+/*
+constraints:
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSubarrays = function(nums, k) {
+
+};
+
+
+const inputDatas = [
+    {data: [], answer: ""},
+];
+
+/*
+LeetCode Hard.
+제출 230.2K, 정답률 51.6%
+*/
+
+for (let i = 0; i < inputDatas.length; i++) {
+    const {data, answer} = inputDatas[i];
+    const res = solution(...data);
+    if (JSON.stringify(res) === JSON.stringify(answer)) {
+        console.log("pass");
+    } else {
+        let summary = "fail";
+        for (const [label, content] of [["expected:", answer], ["got:", res]]) {
+            summary += `\n  ${label}\n`;
+            summary += `    ${content}`;
+            summary = summary.trimEnd();
+        }
+        console.log(summary);
+    }
+}
