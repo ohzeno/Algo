c4aae4b0d7d3e0fe7253b523531b05f827ef2ff4
Zeno Oh <blackroseaaa3@gmail.com>
2024-11-18 21:31:46 +0900

    Programmers Lv.1 repeat

--- Programmers/Previous_KAKAO/Lv.1/신고 결과 받기.py
+++ Programmers/Previous_KAKAO/Lv.1/신고 결과 받기.py
@@ -1,48 +1,62 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/92334
+"""
+constraints:
+  • 2 ≤ id_list의 길이 ≤ 1,000
+    ◦ 1 ≤ id_list의 원소 길이 ≤ 10
+    ◦ id_list의 원소는 이용자의 id를 나타내는 문자열이며 알파벳 소문자로만 이루어져 있습니다.
+    ◦ id_list에는 같은 아이디가 중복해서 들어있지 않습니다.
+  • 1 ≤ report의 길이 ≤ 200,000
+    ◦ 3 ≤ report의 원소 길이 ≤ 21
+    ◦ report의 원소는 "이용자id 신고한id"형태의 문자열입니다.
+    ◦ 예를 들어 "muzi frodo"의 경우 "muzi"가 "frodo"를 신고했다는 의미입니다.
+    ◦ id는 알파벳 소문자로만 이루어져 있습니다.
+    ◦ 이용자id와 신고한id는 공백(스페이스)하나로 구분되어 있습니다.
+    ◦ 자기 자신을 신고하는 경우는 없습니다.
+  • 1 ≤ k ≤ 200, k는 자연수입니다.
+  • return 하는 배열은 id_list에 담긴 id 순서대로 각 유저가 받은 결과 메일 수를 담으면 됩니다.
+"""
+
+
 def solution(id_list, report, k):
-    # 각 유저는 한 번에 한 명 신고 가능
-    # 신고 횟수 제한X
-    # 한 유저를 여러 번 신고해도 1회로 처리됨
-    # k번 이상 신고된 유저는 게시판 이용 정지.
-    # 신고한 모든 유저에게 정지 사실을 메일로 발송
-    # 유저가 신고한 모든 내용 취합 후 마지막에 한꺼번에 게시판 이용 정지 시키면서 메일 발송
-    answer = []
-    user_dic = {
+    user_d = {
         user: {
-            'report': [], 'reported': 0, 'mailed': 0
+            'mailed': 0, 'reported_by': []
         } for user in id_list
     }
-    for r in report:
+    for r in set(report):
         reporter, reported = r.split()
-        # 이미 신고한 유저가 아니라면 기록
-        if reported not in user_dic[reporter]['report']:
-            user_dic[reporter]['report'].append(reported)
-            user_dic[reported]['reported'] += 1
-    for user in id_list:  # 유저 순회
-        # 유저가 신고한 대상들 순회
-        for r in user_dic[user]['report']:
-            # 신고당한 횟수가 k번 이상이면 신고자에게 메일 추가
-            if user_dic[r]['reported'] >= k:
-                user_dic[user]['mailed'] += 1
-        # 최종 메일 개수 정답배열에 추가
-        answer.append(user_dic[user]['mailed'])
-    return answer
+        user_d[reported]['reported_by'].append(reporter)
+    for user in user_d.values():
+        if len(user['reported_by']) >= k:
+            for reporter in user['reported_by']:
+                user_d[reporter]['mailed'] += 1
+    return [user['mailed'] for user in user_d.values()]
 
 inputdatas = [
-    [["muzi", "frodo", "apeach", "neo"],
-     ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"],
-     2],
-    [["con", "ryan"],
-     ["ryan con", "ryan con", "ryan con", "ryan con"],
-     3]
+    {"data": [["muzi", "frodo", "apeach", "neo"],
+              ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"], 2], "answer": [2, 1, 1, 0]},
+    {"data": [["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3], "answer": [0, 0]}
 ]
 
 """
-2022 카카오 공채 기출. Lv.1. 옮겨적기부터 채점까지 18분 걸렸다.
-정확성 테스트에 10초 시간제한이 걸린 특이한 문제.
-그냥 생각나는대로 구현하면 정답이다.
-베스트 풀이를 보니 set를 이용해 중복신고를 제거했다.
-조건을 다시 생각해보면 같은 신고자가 같은 신고대상을 신고한 것이 무효이니 set을 사용하면 편하다.
+2022 KAKAO BLIND RECRUITMENT
+Lv.1. 현 시점 완료한 사람 34,278명, 정답률 39%
+예전에 풀었을 때와 비교하면 answer배열을 사용하지 않고 set을 이용해 중복신고를 제거했다.
+딕셔너리 삽입 순서를 이용하는게 불쾌하면 id_list를 순회하면서 key로 사용해도 된다.
+이 문제 나온지가 언젠데 아직도 정답률이 39%밖에 안된다는게 신기하다.
+베스트 풀이는 여전히 report를 두번 순회하는데, 
+report의 길이를 생각하면 상당히 비효율적이다.
 """
-for t in inputdatas:
-    print(solution(t[0], t[1], t[2]))
+
+for inputdata in inputdatas:
+    data, ans = inputdata["data"], inputdata["answer"]
+    res = solution(*data)
+    if res == ans:
+        print("pass")
+    else:
+        summary = "fail"
+        for label, content in [("expected:", ans), ("got:", res)]:
+            summary += f"\n  {label}\n"
+            summary += f"    {content}"
+            summary = summary.rstrip()
+        print(summary)
