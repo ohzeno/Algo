4e8c807ff56ac8599f61c9e3511eae130a6a00ca
Zeno Oh <blackroseaaa3@gmail.com>
2025-03-30 00:35:11 +0900

    Programmers Lv.2 repeat

--- Programmers/Previous_KAKAO/Lv.2/[1차] 뉴스 클러스터링.py
+++ Programmers/Previous_KAKAO/Lv.2/[1차] 뉴스 클러스터링.py
@@ -6,6 +6,18 @@ constraints:
 
 
 def solution(str1, str2):
+    """
+    J(A, B) = |A ∩ B| / |A ∪ B|
+    둘 다 공집합이면 1
+    중복 허용하는 다중집합에서도 가능.
+    A가 1 3개, B가 5개 갖고있으면
+    |A ∩ B|가 갖는 1은 min(3, 5) = 3
+    |A ∪ B|가 갖는 1은 max(3, 5) = 5
+    각 문자열은 2~1000자
+    두 글자씩 끊어서 다중집합 원소로. ex) FRANCE -> [FR, RA, AN, NC, CE]
+    영문자만 유효. 공백 숫자 특수문자 버림. ex) ab+ -> [ab]
+    자카드 유사도에 65536 곱하고 소수점 아래 버리고 출력.
+    """
     def str2d(s):
         d = {}
         for i in range(len(s) - 1):
@@ -30,9 +42,10 @@ inputdatas = [
 
 """
 2018 KAKAO BLIND RECRUITMENT
-Lv.2. 현 시점 완료한 사람 19,842명, 정답률 62%
+Lv.2. 현 시점 완료한 사람 20,987명, 정답률 63%
 이전엔 1차 35분, 2차 25분 걸렸었는데 이번엔 15분 걸렸다.
 풀고 나서 풀이를 좀 간결하게 바꿨다.
+4차 8분. 코드 달라진 건 거의 없다.
 """
 
 for inputdata in inputdatas:
