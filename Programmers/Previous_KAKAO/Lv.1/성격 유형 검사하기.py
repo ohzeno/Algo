181deb791e412a437b6ab23f60601a4741367212
Zeno Oh <blackroseaaa3@gmail.com>
2025-04-23 14:28:52 +0900

    Programmers Lv.1 repeat

--- Programmers/Previous_KAKAO/Lv.1/성격 유형 검사하기.py
+++ Programmers/Previous_KAKAO/Lv.1/성격 유형 검사하기.py
@@ -1,47 +1,38 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/118666
+"""
+constraints:
+  • 1 ≤ survey의 길이 ( = n) ≤ 1,000
+    ◦ survey의 원소는 "RT", "TR", "FC", "CF", "MJ", "JM", "AN", "NA" 중 하나입니다.
+    ◦ survey[i]의 첫 번째 캐릭터는 i+1번 질문의 비동의 관련 선택지를 선택하면 받는 성격 유형을 의미합니다.
+    ◦ survey[i]의 두 번째 캐릭터는 i+1번 질문의 동의 관련 선택지를 선택하면 받는 성격 유형을 의미합니다.
+  • choices의 길이 = survey의 길이
+    ◦ choices[i]는 검사자가 선택한 i+1번째 질문의 선택지를 의미합니다.
+    ◦ 1 ≤ choices의 원소 ≤ 7
+"""
+from collections import defaultdict
+
 def solution(survey, choices):
-    """
-    1: RT, 2: CF, 3: JM, 4: AN
-    총 n개 질문, 7개 선택지.
-    1~7 예시 1번지표면 R3~0~T3 점수
-    RT가 아니라 TR처럼 뒤집어져 나올 수도 있음.
-    하나의 지표에서 양쪽 점수가 같으면 사전순으로 빠른 성격 유형을 선택.
-    질문별 지표가 담긴 survey, 질문별 선택지가 담긴 choices가 주어질 때,
-    성격 유형을 지표 번호 순서대로 return
-    """
-    mbti = {}
-    for cha, chose in zip(survey, choices):
-        a, b = cha
-        mbti.setdefault(a, 0)
-        mbti.setdefault(b, 0)
-        if chose == 1:
-            mbti[a] += 3
-        elif chose == 2:
-            mbti[a] += 2
-        elif chose == 3:
-            mbti[a] += 1
-        elif chose == 5:
-            mbti[b] += 1
-        elif chose == 6:
-            mbti[b] += 2
-        elif chose == 7:
-            mbti[b] += 3
+    points = defaultdict(int)
+    for mbti, choice in zip(survey, choices):
+        if choice == 4:
+            continue
+        l, r = mbti
+        if choice < 4:
+            points[l] += 4 - choice
+        else:
+            points[r] += choice - 4
     ans = ''
-    for a, b in ["RT", "CF", "MJ", "AN"]:
-        pa, pb = mbti.get(a, 0), mbti.get(b, 0)
-        if pa > pb:
-            ans += a
-        elif pa < pb:
-            ans += b
-        else:  # min이 글자도 사전순 정렬해줌.
-            ans += min(a, b)
+    for pset in ["RT", "CF", "JM", "AN"]:
+        l, r = pset
+        if points[l] >= points[r]:
+            ans += l
+        else:
+            ans += r
     return ans
 
 inputdatas = [
-    [["AN", "CF", "MJ", "RT", "NA"],
-     [5, 3, 2, 7, 5]],
-    [["TR", "RT", "TR"],
-     [7, 1, 3]]
+    {"data": [["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]], "answer": "TCMA"},
+    {"data": [["TR", "RT", "TR"], [7, 1, 3]], "answer": "RCJA"}
 ]
 
 """
@@ -51,6 +42,21 @@ inputdatas = [
 
 2차 시도. 12분 걸렸다. JM을 미리 정렬하지 않고 min을 사용했다.
 setdefault, get, zip을 사용해 몇 줄 짧아졌다.
+
+3차 시도. 
+포인트를 if문이 아니라 계산 방식으로 바꿨고 
+setdefault 대신 defaultdict를 사용해
+훨씬 짧아졌다.
 """
-for t in inputdatas:
-    print(solution(t[0], t[1]))
+for inputdata in inputdatas:
+    data, ans = inputdata["data"], inputdata["answer"]
+    res = solution(*data)
+    if res == ans:
+        print("pass")
+    else:
+        summary = "fail"
+        for label, content in [("expected:", ans), ("got:", res)]:
+            summary += f"\n  {label}\n"
+            summary += f"    {content}"
+            summary = summary.rstrip()
+        print(summary)
