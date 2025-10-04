34530869010f235881398c4070553478ae9a15f2
오제노 <blackroseaaa3@gmail.com>
2025-10-04 18:55:22 +0900

    Refactor: Import only run_judge from boj_judge_utils

--- Utils/boj_judge.py
+++ Utils/boj_judge.py
@@ -1,13 +1,11 @@
-from Utils.boj_judge_utils import *
+from Utils.boj_judge_utils import run_judge
 
 
 inputdatas = [
-    {"data": """25
-17
-31
-0""", "answer": """25
-25 12 18 15 16 17
-25 38 31"""}
+    {"data": """3 10
+1 2 3 4 5 6 7 8 9 10""", "answer": """8"""},
+    {"data": """4 3
+10 10 15""", "answer": """7"""}
 ]
 
 run_judge(inputdatas)
