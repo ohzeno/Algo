24b7d02c986ec4f7eadb191a6aaf1c23f2139b35
오제노 <blackroseaaa3@gmail.com>
2026-02-04 14:23:17 +0900

    Programmers Lv.2

---
+++ Programmers/NonCompany/Lv.2/JadenCase 문자열 만들기/JadenCase 문자열 만들기_capitalize.py
@@ -0,0 +1,43 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12951
+"""
+constraints:
+
+"""
+
+
+def solution(s):
+    ss = s.split(" ")
+    res = []
+    for word in ss:
+        if len(word) == 0:
+            res.append(word)
+            continue
+        word = word.capitalize()
+        res.append(word)
+    return " ".join(res)
+
+
+inputdatas = [
+    {"data": ["3people unFollowed me"], "answer": "3people Unfollowed Me"},
+    {"data": ["for the last week"], "answer": "For The Last Week"}
+]
+
+
+"""
+연습문제
+Lv.2. 현 시점 완료한 사람 44,486명, 정답률 80%
+capitalize 존재를 지금 알았다.
+"""
+
+for inputdata in inputdatas:
+    data, ans = inputdata["data"], inputdata["answer"]
+    res = solution(*data)
+    if res == ans:
+        print("pass")
+    else:
+        summary = "fail"
+        for label, content in [("expected:", ans), ("got:", res)]:
+            summary += f"\n  {label}\n"
+            summary += f"    {content}"
+            summary = summary.rstrip()
+        print(summary)
