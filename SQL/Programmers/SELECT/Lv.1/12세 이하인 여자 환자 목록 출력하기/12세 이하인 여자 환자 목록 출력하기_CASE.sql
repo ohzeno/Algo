88a54f37fe70d2e0c9b7326f7ccb80602373e3b7
오제노 <blackroseaaa3@gmail.com>
2026-02-28 12:19:17 +0900

    SQL SELECT Lv.1

---
+++ SQL/Programmers/SELECT/Lv.1/12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기_CASE.sql
@@ -0,0 +1,25 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/132201
+/*
+PATIENT
+    PT_NO	VARCHAR(10)	FALSE
+    PT_NAME	VARCHAR(20)	FALSE
+    GEND_CD	VARCHAR(1)	FALSE
+    AGE	INTEGER	FALSE
+    TLNO	VARCHAR(50)	TRUE
+*/
+
+
+SELECT PT_NAME, PT_NO, GEND_CD, AGE,
+       CASE
+           WHEN TLNO IS NULL THEN 'NONE'
+           ELSE TLNO
+       END
+FROM PATIENT
+WHERE AGE <= 12 AND GEND_CD = 'W'
+ORDER BY AGE DESC, PT_NAME ASC;
+
+
+/*
+SELECT
+Lv.1. 현 시점 완료한 사람 39,832명, 정답률 91%
+*/
