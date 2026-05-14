1ec166b37b47640800b6056c47b60b26c4bb5284
오제노 <blackroseaaa3@gmail.com>
2026-05-15 04:07:55 +0900

    Correct flawed recurrence derivation and update Drunk Passenger function

--- BaekJoon/Math/Probability Theory/Gold/G5/Drunk Passenger/Drunk Passenger.py
+++ BaekJoon/Math/Probability Theory/Gold/G5/Drunk Passenger/Drunk Passenger.py
@@ -1,6 +1,6 @@
 # https://www.acmicpc.net/problem/23337
 import sys
-# sys.stdin = open('input.txt')
+sys.stdin = open('input.txt')
 def input():
     return sys.stdin.readline().rstrip()
 """
@@ -11,20 +11,29 @@ drunk passenger problem 변형.
 n번 승객이 자신의 자리에 앉지 못할 확률을 구하시오.
 """
 
-# def drunk_passenger(n):
-#     if n == 1:
-#         return 1
-#     if n == 2:
-#         return 0.5
-#     denom = 1 + (n - 2) * drunk_passenger(n - 1)
-#     return denom/n
-#
-# def custom(n):
-#     denom = 1 + (n - 2) * drunk_passenger(n - 1)
-#     return denom/(n - 1)
+def drunk_passenger(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 0.5
+    numer = 1
+    for i in range(2, n):
+        numer += drunk_passenger(n - i + 1)
+    return numer / n
+
+def custom_success(n):
+    if n == 2:
+        return 0
+    numer = 0
+    for i in range(2, n):
+        numer += drunk_passenger(n - i + 1)
+    return numer / (n - 1)
+
+def custom_failure(n):
+    return 1 - custom_success(n)
 
 n = int(input())
-print((1+(n-2)*0.5)/(n-1))
+print(custom_failure(n))
 
 
 """
@@ -34,8 +43,4 @@ drunk passenger problem 공부하다가 점화식 이해하고 나서 풀었다.
 위의 함수들은 조건부확률로 점화식 구한 케이스다. 
 custom은 1번 승객이 무조건 다른 사람 자리 앉는다는 조건 때문에
 기존 식에서 확률이 좀 달라져서 추가했다.
-그런데 drunk_passenger는 n이 2 이상일 때 전부 0.5다.
-이 문제의 n은 2 이상이니 P(1)이 되는 n=2가 문제인데,
-denom에서 P(n-1)의 계수가 (n-2)라 n=2일 때 P는 사라진다.
-그래서 그냥 입력값 식에서 P를 0.5로 두고 바로 출력해도 된다.
 """
